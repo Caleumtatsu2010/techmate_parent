207aab0b33afedcfe7fc65408c49ed16f85a9a63
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2023-06-26 00:28:32 +0700

    .

--- techmate_session/src/main/java/com/caleumtatsu2010/techmate_session/session/http/config/SessionConfigPath.java
+++
@@ -1,9 +0,0 @@
-package com.caleumtatsu2010.techmate_session.session.http.config;
-
-public class SessionConfigPath {
-	private static String dir = "C:\\stuff\\Projects\\techmate_parent";
-	
-	private static String resourceDir = dir + "\\techmate_session\\src\\main\\resources\\";
-	
-	public static String path = resourceDir + "SessionConfig.properties";
-}
